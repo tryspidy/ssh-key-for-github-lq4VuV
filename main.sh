38a73b741c2d0bbb6d805a74a38272b355081fd0
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-11 10:34:11 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,11 @@
+SSH stands for Secure Shell.
+When working with a GitHub repository,
+you'll often need to introduce yourself to
+GitHub using your username and password.
+SSH key is an alternate way to identify yourself
+that doesn't require you to enter you username and password every time.
+
+SSH keys come in pairs, a public key that
+gets shared with services like GitHub,
+and a private key that is stored only on
+your computer. If the keys match, you're granted access.
